dc37a2847f49fe545ac29cc9eec7971ad9e9dda9
aolindahl <alindahl@dhcp-visitor-221-49.slac.stanford.edu>
2014-11-24 15:57:20 -0800

    Some more plotting

--- plotting.py
+++ plotting.py
@@ -7,24 +7,28 @@ print 'Avaliablea data:'
 for k in f:
     print '\t{}'.format(k)
 
-every = 1000
+every = 10
 sl = slice(None, None, every)
-
+Isl = (f['eBeamCurrentBC2_A'][sl] < 2e5) \
+    & (f['eBeamEnergyL3_MeV'][sl] > 4500) \
+    & (f['eBeamEnergyBC2_MeV'][sl] > 4960) \
+#    & (f['eBeamEnergyL3_MeV'][sl] - f['eBeamEnergyBC2_MeV'][sl] > -465) \
+#    & (f['eBeamEnergyL3_MeV'][sl] - f['eBeamEnergyBC2_MeV'][sl] < -447)
 
 figure(1); clf()
 xlabel('BC2 energy (MeV)')
 ylabel('L3 energy (MeV)')
-plot(f['eBeamEnergyBC2_MeV'][sl], f['eBeamEnergyL3_MeV'][sl], '.')
+plot(f['eBeamEnergyBC2_MeV'][sl][Isl], f['eBeamEnergyL3_MeV'][sl][Isl], '.')
 
 figure(2); clf()
 xlabel('BC2 energy (MeV)')
 ylabel('L3 energy - BC2 energy (MeV)')
-plot(f['eBeamEnergyBC2_MeV'][sl], f['eBeamEnergyL3_MeV'][sl] - f['eBeamEnergyBC2_MeV'][sl], '.')
+plot(f['eBeamEnergyBC2_MeV'][sl][Isl], f['eBeamEnergyL3_MeV'][sl][Isl] - f['eBeamEnergyBC2_MeV'][sl][Isl], '.')
 
 
 figure(3); clf()
 xlabel('BC2 energy (MeV)')
-xlabel('BC2 current (A)')
-plot(f['eBeamEnergyBC2_MeV'][sl], f['eBeamCurrentBC2_A'][sl], '.')
+ylabel('BC2 current (A)')
+plot(f['eBeamEnergyBC2_MeV'][sl][Isl], f['eBeamCurrentBC2_A'][sl][Isl], '.')
 
 f.close()
