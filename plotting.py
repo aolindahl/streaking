ef7a4a2f5011677270dc2e73d4d9787868da5b52
Anton O. Lindahl <aolindahl@gmail.com>
2014-11-24 14:55:41 -0800

    First versionof plotting tool.

---
+++ plotting.py
@@ -0,0 +1,30 @@
+from pylab import *
+ion()
+import h5py
+
+f = h5py.File('run24_all.h5', 'r')
+print 'Avaliablea data:'
+for k in f:
+    print '\t{}'.format(k)
+
+every = 1000
+sl = slice(None, None, every)
+
+
+figure(1); clf()
+xlabel('BC2 energy (MeV)')
+ylabel('L3 energy (MeV)')
+plot(f['eBeamEnergyBC2_MeV'][sl], f['eBeamEnergyL3_MeV'][sl], '.')
+
+figure(2); clf()
+xlabel('BC2 energy (MeV)')
+ylabel('L3 energy - BC2 energy (MeV)')
+plot(f['eBeamEnergyBC2_MeV'][sl], f['eBeamEnergyL3_MeV'][sl] - f['eBeamEnergyBC2_MeV'][sl], '.')
+
+
+figure(3); clf()
+xlabel('BC2 energy (MeV)')
+xlabel('BC2 current (A)')
+plot(f['eBeamEnergyBC2_MeV'][sl], f['eBeamCurrentBC2_A'][sl], '.')
+
+f.close()
