f4f1bcd6e66dd14951241a40ccf63bd8303201b4
Anton O. Lindahl <aolindahl@gmail.com>
2014-11-25 08:43:35 -0800

    FEE added in lcls.py

--- lcls.py
+++ lcls.py
@@ -13,6 +13,12 @@ EBeamTypeList = (psana.Bld.BldDataEBeamV0,
 _EBeamType = None
 _EBeamSource = psana.Source('BldInfo(EBeam)')
 
+feeTypeList = (psana.Bld.BldDataFEEGasDetEnergy,
+        psana.Bld.BldDataFEEGasDetEnergyV1)
+
+_feeType = None
+_feeSource = psana.Source('BldInfo(FEEGasDetEnergy)')
+
 def getEBeamEnergyL3_MeV(evt, verbose=False):
     EBeamObject = getEBeamObject(evt, verbose)
     if EBeamObject == None:
@@ -62,6 +68,36 @@ def _determineEBeamType(evt, verbose=False):
         elif verbose:
             print ' wrong one.'
 
+def getPulseEnergy_mJ(evt):
+    fee = getFeeObject(evt)
+    if fee is None:
+        return np.array([np.nan for i in range(4)])
+    return np.array( [ fee.f_11_ENRC(),
+            fee.f_12_ENRC(),
+            fee.f_21_ENRC(),
+            fee.f_22_ENRC()] )
+
+def getFeeObject(evt, verbose=False):
+    if _feeType is None:
+        _determineFeeType(evt, verbose)
+    return evt.get(_feeType, _feeSource)
+
+def _determineFeeType(evt, verbose=False):
+    global _feeType
+    if verbose:
+        print 'Find the correct fee type.'
+    for type in feeTypeList:
+        if verbose:
+            print 'Trying {};'.format(type),
+        data = evt.get(type, _feeSource)
+        if data is not None:
+            _feeType = type
+            if verbose:
+                print ' correct.'
+            break
+        elif verbose:
+            print ' wrong one.'
+
 if __name__ == '__main__':
     ds = psana.DataSource('exp=amoc8114:run=24')
     evt = ds.events().next()
@@ -73,4 +109,5 @@ if __name__ == '__main__':
         verbose=True))
     print 'I is {} A'.format(getEBeamPkCurrentBC2_A(evt,
         verbose=True))
+    print 'fee is {} mJ'.format(getPulseEnergy_mJ(evt))
 
